14fb026204fa6b905b330a5fd27a52ac2adc8335
Joe Lin <joelin3478@gmail.com>
2023-02-06 15:17:05 -0500

    Add resolvers for getProductInfo and getProductStyles

--- src/graphql/resolvers.ts
+++ src/graphql/resolvers.ts
@@ -1,9 +1,17 @@
 import { Product } from "../models/products";
 export const resolvers = {
   Query: {
-    async getProducts (id: number) {
+    async getProducts (id: string) {
       const productID = Number(id)
-      return await Product.find({ id: productID }, {}, { hint: 'id_1' })
+      return await Product.find({ id: productID }, {}, { hint: 'id_1' }).lean()
+    },
+    async getProductInfo (id: string) {
+      const productID = Number(id)
+      return await Product.find({ id: productID }, {}, { hint: 'id_1' }).lean()
+    },
+    async getProductStyles(id: string) {
+      const productID = Number(id)
+      return await Product.find({ id: productID }, {}, { hint: 'id_1' }).lean()
     }
   },
 };
